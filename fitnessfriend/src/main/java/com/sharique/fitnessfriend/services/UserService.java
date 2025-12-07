af9dff885170bcdb2834d4aa302abfb645f572b8
sharique ahmad <ahmedsharique250@gmail.com>
2025-12-07 12:51:46 +0530

    write the db service to save user

--- fitnessfriend/src/main/java/com/sharique/fitnessfriend/services/UserService.java
+++ fitnessfriend/src/main/java/com/sharique/fitnessfriend/services/UserService.java
@@ -16,13 +16,31 @@ public class UserService {
 
   private final  UserRepository repository;
   public UserResponse register(RegisterRequest request) {
+
+    if(repository.existsByEmail(request.getEmail())){
+      throw new RuntimeException("Email already exist");
+
+
+
+    }
     User user=new User();
     user.setEmail(request.getEmail());
     user.setFirstName(request.getFirstName());
     user.setLastName(request.getLastName());
-    user.setPassword(user.getPassword());
-     User savedUser=new User();
-     UserResponse response =new UserResponse();
+    user.setPassword(request.getPassword());
+     User savedUser=repository.save(user);
+     UserResponse userResponse =new UserResponse();// userresposne ka object banaya hai yaha pe
+     userResponse.setId(savedUser.getId());
+     userResponse.setPassword(savedUser.getPassword());
+     userResponse.setEmail(savedUser.getEmail());
+     userResponse.setFirstName(savedUser.getFirstName());   
+      userResponse.setLastName(savedUser.getLastName());  
+      userResponse.setCreatedAt(savedUser.getCreatedAt());
+      userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+     return userResponse;
+
+
+
   }
 
 }
