71e387be276e7d8f918e792c64773518db1efb66
sharique ahmad <ahmedsharique250@gmail.com>
2025-12-09 10:32:22 +0530

    endpoints for trackactivity

---
+++ activityservice/src/main/java/com/sharique/activityservice/controller/ActivityController.java
@@ -0,0 +1,25 @@
+package com.sharique.activityservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sharique.activityservice.dto.ActivityRequest;
+import com.sharique.activityservice.dto.ActivityResponse;
+import com.sharique.activityservice.service.ActivityService;
+
+import lombok.AllArgsConstructor;
+@RestController
+@RequestMapping("/api/activites")
+@AllArgsConstructor
+public class ActivityController {
+  private ActivityService activityService;
+
+  @PostMapping
+  public ResponseEntity<ActivityResponse> trackActiviy(@RequestBody ActivityRequest request){
+
+  return ResponseEntity.ok(activityService.trackActivity(request));
+
+}}
