1025e465bc27945939b1d0a75ab2283bc87beb8c
sharique ahmad <ahmedsharique250@gmail.com>
2025-12-13 02:29:48 +0530

    changes

---
+++ aiservice/src/main/java/com/sharique/aiservice/model/ActivityType.java
@@ -0,0 +1,16 @@
+package com.sharique.aiservice.model;
+
+public enum ActivityType {
+  RUNNING,
+  WALKING,
+  CYCLING,
+  SWIMMING,
+  WEIGHT_TRAINING,
+  YOGA,
+  HIIT,
+  CARDIO,
+  STRECHING,
+  OTHER
+
+
+}
